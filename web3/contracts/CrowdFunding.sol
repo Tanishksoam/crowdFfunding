e832cbe4b4974980fa018f047d916f387443caaa
Tanishksoam <soamtanishk@gmail.com>
2024-05-11 01:09:17 +0530

    dontations function created

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 contract CrowdFunding {
-    struct Campaign{
+    struct Campaign {
         address owner;
         string title;
         string description;
@@ -18,16 +18,54 @@ contract CrowdFunding {
 
     uint256 public NoOfCampaigns = 0;
 
-    function createCampaign(address _owner, string memory _title , string memory _description ,uint256 _target, uint256 _deadeline , string memory _image ) public {
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadeline,
+        string memory _image
+    ) public returns (uint256) {
         Campaign storage newCampaign = campaigns[NoOfCampaigns];
+
+        require(
+            newCampaign.deadline < block.timestamp,
+            "Deadline should be in future"
+        );
+
+        newCampaign.owner = _owner;
+        newCampaign.title = _title;
+        newCampaign.description = _description;
+        newCampaign.target = _target;
+        newCampaign.deadline = _deadeline;
+        newCampaign.image = _image;
+        newCampaign.collected = 0;
+
+        NoOfCampaigns++;
+        return NoOfCampaigns - 1;
     }
 
-    function donateToCampaign(){}
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
 
-    function getDonators(){}
+        Campaign storage selectedCampaign = campaigns[_id];
 
-    function getCampaigns(){}
+        require(
+            selectedCampaign.deadline > block.timestamp,
+            "Campaign is expired"
+        );
 
+        selectedCampaign.donators.push(msg.sender);
+        selectedCampaign.donations.push(amount);
 
+        (bool sent,) = selectedCampaign.owner.call{value: amount}("");
 
+        if(sent) {
+            selectedCampaign.collected += amount;
+        }
+    }
+
+    function getDonators() {}
+
+    function getCampaigns() {}
 }
